42b9fc91123a42c9ccf9b35433a3a31e28a02d1b
Weili Yi <yi@weiliy.net>
2016-10-31 01:36:38 +0800

    feat: got place list from server

--- app.js
+++ app.js
@@ -1,43 +1,11 @@
-var places = [
-  'Victoria Peak',
-  'Ocean Park Hong Kong',
-  'Hong Kong Disneyland',
-  'Lantau Island',
-  'Tian Tan Buddha',
-  'Ngong Ping 360',
-  'Po Lin Monastery',
-  'Happy Valley Racecourse',
-  'Hong Kong Museum of History',
-  'Lamma Island',
-  'Repulse Bay',
-  'Cheung Chau',
-  'Man Mo Temple',
-  'Tung Choi Street',
-  'Wong Tai Sin Temple',
-  'Ten Thousand Buddhas Monastery',
-  'Clock Tower, Hong Kong',
-  'Hong Kong Space Museum',
-  'Hong Kong Wetland Park',
-  'Hong Kong Heritage Museum',
-  'Mademe Tussauds Hong Kong',
-  'Hong Kong Science Museum',
-  'Hong Kong Zoological and Botanical Gardens',
-  'Hong Kong Cultural Centre',
-  'Bank of China Tower'
-]
-
 var NeighorhoodViewModel = function() {
   var self = this;
   self.places = ko.observableArray([]);
 
-  self.places(places);
-
-  if (!google) {
-    alert("google is not found!");
-  } else {
-    console.log("google is found!");
-  }
-}
+  $.getJSON('/data.json', function(d) {
+    self.places(d.payload.places);
+  });
+};
 
 function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
@@ -45,5 +13,5 @@ function initMap(){
     zoom: 12
   });
   ko.applyBindings(new NeighorhoodViewModel());
-}
+};
 
